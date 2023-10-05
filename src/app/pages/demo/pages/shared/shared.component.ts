f511a06980e19d321efd69dbbe4535d35e3cd9a3
Nicolas Peña <103202106+Nicolasp-dev@users.noreply.github.com>
2023-10-05 12:08:19 -0500

    Feat: Checkboxes module added

--- src/app/pages/demo/pages/shared/shared.component.ts
+++ src/app/pages/demo/pages/shared/shared.component.ts
@@ -45,7 +45,14 @@ export class SharedComponent implements OnInit {
         },
       ],
       password: [null, { updateOn: 'blur', validators: [Validators.required] }],
-      select: [null, { updateOn: 'blur', validators: Validators.required }],
+      select: [null, { updateOn: 'change', validators: Validators.required }],
+      checkboxes: [
+        null,
+        {
+          updateOn: 'change',
+          validators: Validators.required,
+        },
+      ],
     });
   }
 
