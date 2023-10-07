07cc6c78d220233944417dd118674a347bfda697
Nicolas Peña <103202106+Nicolasp-dev@users.noreply.github.com>
2023-10-07 18:24:44 -0500

    redcuers fixed

--- src/app/store/dictionaries/dictionaries.reducer.ts
+++ src/app/store/dictionaries/dictionaries.reducer.ts
@@ -2,38 +2,36 @@ import { Dictionaries } from './dictionaries.models';
 import * as fromActions from './dictionaries.actions';
 
 export interface DictionariesState {
-    entities: Dictionaries;
-    loading: boolean;
-    error: string;
+  entities: Dictionaries | null;
+  loading: boolean;
+  error: string;
 }
 
 const initialState: DictionariesState = {
-    entities: null,
-    loading: null,
-    error: null
+  entities: null,
+  loading: false,
+  error: '',
 };
 
 export function reducer(
-    state = initialState,
-    action: fromActions.All
+  state = initialState,
+  action: fromActions.All
 ): DictionariesState {
-    switch (action.type) {
-
-        case fromActions.Types.READ: {
-            return { ...state, loading: true, error: null };
-        }
-
-        case fromActions.Types.READ_SUCCESS: {
-            return { ...state, entities: action.dictionaries, loading: false };
-        }
-
-        case fromActions.Types.READ_ERROR: {
-            return { ...state, entities: null, loading: false, error: action.error };
-        }
-
-        default: {
-            return state;
-        }
-
+  switch (action.type) {
+    case fromActions.Types.READ: {
+      return { ...state, loading: true, error: '' };
     }
+
+    case fromActions.Types.READ_SUCCESS: {
+      return { ...state, entities: action.dictionaries, loading: false };
+    }
+
+    case fromActions.Types.READ_ERROR: {
+      return { ...state, entities: null, loading: false, error: action.error };
+    }
+
+    default: {
+      return state;
+    }
+  }
 }
