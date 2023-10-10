20a43586a4b40a00303f9b5a133f8cda0e865a8d
Nicolas Peña <103202106+Nicolasp-dev@users.noreply.github.com>
2023-10-09 21:31:42 -0500

    Auth: User.models

--- src/app/models/backend/user/index.ts
+++ src/app/models/backend/user/index.ts
@@ -1,15 +1,17 @@
 // import { firestore } from 'firebase/app';
 import { Employee, Recruiter } from './roles';
 
+export * from './roles';
+
 export interface User {
-    uid: string;
-    name: string;
-    photoURL: string;
-    email: string;
-    country: string;
-    about?: string;
-    roleId: string;
-    role: Employee | Recruiter;
-    created: any;
-    updated?: any;
+  uid: string;
+  name: string;
+  photoURL: string;
+  email: string;
+  country: string;
+  about?: string;
+  roleId: string;
+  role: Employee | Recruiter;
+  created: any;
+  updated?: any;
 }
